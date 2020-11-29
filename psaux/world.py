5958a970be9cca9854743511cfd68fc07c0a401a
anderslindho <anders.lindh.olsson@gmail.com>
2020-11-29 23:35:50 +0100

    refactor spawning initial planets

--- psaux/world.py
+++ psaux/world.py
@@ -22,6 +22,15 @@ class World:
         self.entities = list()
         self.entity_batch = pyglet.graphics.Batch()
 
+        logging.debug(
+            f"World created at {self.real_time=}, {self.physics_time=} with {len(self.entities)} entities\n"
+            f"{vars(self.settings)}"
+        )
+
+        self.sun = None
+        self.spawn_initial_planets()
+
+    def spawn_initial_planets(self):
         # create sun
         self.sun = Circle(
             position=Vector3([PsauxConfig.width / 2.0, PsauxConfig.height / 2.0, 0.0]),
@@ -33,29 +42,8 @@ class World:
         self.entities.append(self.sun)
 
         # create starting planets
-        self.entities.append(
-            Circle(
-                position=Vector3([200.0, 700.0, 0.0]),
-                mass=1e4,
-                velocity=Vector3([3e-4, 4e-5, 0.0]),
-                color=GREEN,
-                batch=self.entity_batch,
-            )
-        )
-        self.entities.append(
-            Circle(
-                position=Vector3([400.0, 480.0, 0.0]),
-                mass=1e4,
-                velocity=Vector3([5e-4, 2e-4, 0.0]),
-                color=GREEN,
-                batch=self.entity_batch,
-            )
-        )
-
-        logging.debug(
-            f"World created at {self.real_time=}, {self.physics_time=} with {len(self.entities)} entities\n"
-            f"{vars(self.settings)}"
-        )
+        self.spawn_planet(200.0, 700.0, 3e-4, 4e-5, 1e4, GREEN)
+        self.spawn_planet(400.0, 480.0, 5e-4, 2e-4, 1e4, GREEN)
 
     def update(self, delta_time: float) -> None:
         time_step = delta_time * self.settings.time_warp_factor
@@ -78,24 +66,30 @@ class World:
             f"{len(self.entities)} existing at {self.real_time=}, {self.physics_time=}"
         )
 
+    def draw(self):
+        self.entity_batch.draw()
+
     def modify_time_warp(self, change: float) -> None:
         self.settings.time_warp_factor += change * self.settings.time_warp_multiplier
 
         logging.debug(f"Changing time warp to {self.settings.time_warp_factor}")
 
-    def draw(self):
-        self.entity_batch.draw()
-
+    # todo: accept vectors instead
     def spawn_planet(
-        self, x: float, y: float, velocity_right: float, velocity_up: float
+        self,
+        x: float,
+        y: float,
+        velocity_right: float,
+        velocity_up: float,
+        mass: float = 1e2,
+        color: tuple = BLUE,
     ):
-        mass = 1e2
         velocity = Vector3([velocity_right, velocity_up, 0.0])
         planet = Circle(
             position=Vector3([x, y, 0.0]),
             mass=mass,
             velocity=velocity,
-            color=BLUE,
+            color=color,
             batch=self.entity_batch,
         )
         self.entities.append(planet)
