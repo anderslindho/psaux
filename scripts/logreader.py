8ef5624a6794782f5a3118edcd127aab0b86e862
anderslindho <anders.lindh.olsson@gmail.com>
2020-11-30 17:19:29 +0100

    extract forces in logreader

--- scripts/logreader.py
+++ scripts/logreader.py
@@ -4,7 +4,7 @@ import re
 
 def get_data(filename: str = None) -> list:
     pattern = re.compile(
-        r"^(\d*.\d*).+(?<=Object )(\d+).+(?<=self.position=)\w*\((\[[0-9.,\- e+]+])\).+\((\[[0-9.e\-, +]+\]*)"
+        r"^(\d*.\d*).+(?<=Object )(\d+).+(?<=self.position=)\w*\((\[[0-9.,\- e+]+])\).+\((\[[0-9.e\-, +]+\]*).+(?<=experiencing )([0-9e.-]+)"
     )
     # takes timestamp, id, position, and velocity from the following string:
     # 1606168027.927264 - DEBUG - Object 3 at self.position=Vector3([428.23973285, 324.60161277,   0.        ]) with self.velocity=Vector3([-0.00059675,  0.00025162,  0.        ]) experiencing 4.426041576865288e-05 forces
@@ -20,6 +20,7 @@ def get_data(filename: str = None) -> list:
             ids = int(m[2])
             positions = ast.literal_eval(m[3])
             velocities = ast.literal_eval(m[4])
-            dataset.append([time, positions, velocities, ids])
+            forces = float(m[5])
+            dataset.append([time, positions, velocities, ids, forces])
 
     return dataset
